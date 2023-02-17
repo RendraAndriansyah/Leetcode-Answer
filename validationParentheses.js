50379354f1dfbadddc267a8b4428b6142b20032e
travelite <rendrabootcamp@gmail.com>
2023-02-17 16:18:33 +0700

    validationParentheses

---
+++ validationParentheses.js
@@ -0,0 +1,21 @@
+var isValid = function (s) {
+  let stack = [];
+  for (let idx = 0; idx < s.length; idx++) {
+    if (s[idx] == "{") {
+      stack.push("}");
+    } else if (s[idx] == "[") {
+      stack.push("]");
+    } else if (s[idx] == "(") {
+      stack.push(")");
+    } else if (stack.pop() !== s[idx]) {
+      return false;
+    }
+  }
+  return !stack.length;
+};
+
+console.log(isValid("{}()"));
+console.log(isValid("()"));
+console.log(isValid("{[]}"));
+console.log(isValid("{)"));
+console.log(isValid("{)()("));
