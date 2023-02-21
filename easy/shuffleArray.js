aff9abda0cea1a0bed639c4f8d230032e38adfc9
travelite <rendrabootcamp@gmail.com>
2023-02-21 14:45:13 +0700

    shuffle array

---
+++ easy/shuffleArray.js
@@ -0,0 +1,11 @@
+var shuffle = function (nums, n) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i >= n) break;
+    result.push(nums[i], nums[i + n]);
+  }
+  return result;
+};
+
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
+console.log(shuffle([1, 1, 2, 2], 2));
