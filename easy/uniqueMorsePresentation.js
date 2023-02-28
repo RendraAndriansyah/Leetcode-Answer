ea18f7d803d6e4464e641e4bfdbab1f2b7a95ee1
travelite <rendrabootcamp@gmail.com>
2023-02-28 18:11:57 +0700

    test

--- easy/uniqueMorsePresentation.js
+++ easy/uniqueMorsePresentation.js
@@ -1,29 +1,47 @@
-const morse = [
-  ".-",
-  "-...",
-  "-.-.",
-  "-..",
-  ".",
-  "..-.",
-  "--.",
-  "....",
-  "..",
-  ".---",
-  "-.-",
-  ".-..",
-  "--",
-  "-.",
-  "---",
-  ".--.",
-  "--.-",
-  ".-.",
-  "...",
-  "-",
-  "..-",
-  "...-",
-  ".--",
-  "-..-",
-  "-.--",
-  "--..",
-];
-var uniqueMorseRepresentations = function (words) {};
+var uniqueMorseRepresentations = function (words) {
+  const morse = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+
+  let transformations = new Set();
+
+  for (let word of words) {
+    let trans = "";
+    for (let letter of word) {
+      let index = letter.charCodeAt() - 97;
+      trans += morse[index];
+    }
+
+    transformations.add(trans);
+  }
+
+  return transformations.size;
+};
+
+console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]));
+console.log(uniqueMorseRepresentations(["rwjje", "aittjje", "auyyn", "lqtktn", "lmjwn"]));
