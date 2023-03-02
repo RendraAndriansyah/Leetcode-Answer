919eef4c816b6d1bbb15fa0ac5b59a33cd3ee57e
travelite <rendrabootcamp@gmail.com>
2023-03-03 02:04:38 +0700

    learn sorting

--- easy/sortPeople.js
+++ easy/sortPeople.js
@@ -1,6 +1,14 @@
 var sortPeople = function (names, heights) {
+  // with object temporary
+  let obj = [];
+  for (let i = 0; i < names.length; i++) {
+    obj.push({ names: names[i], heights: heights[i] });
+  }
+  return obj.sort((a, b) => b.heights - a.heights).map((item) => item.names);
+
+  // ~> using hashMap ES6
   return names
-    .map((name, i) => ({ name: names[i], heights: heights[i] }))
+    .map((x, i) => ({ name: names[i], heights: heights[i] }))
     .sort((a, b) => b.heights - a.heights)
     .map((x) => x.name);
 };
