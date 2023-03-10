99e135541ac09b03816bed477dfd16ee73206ea0
travelite <rendrabootcamp@gmail.com>
2023-03-10 07:18:30 +0700

    reverse string

---
+++ easy/reverseString.js
@@ -0,0 +1,15 @@
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left <= right) {
+    [s[left], s[right]] = [s[right], s[left]];
+    left++;
+    right--;
+  }
+  return s;
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
+console.log(reverseString(["h", "e", "l", "l", "o"]));
