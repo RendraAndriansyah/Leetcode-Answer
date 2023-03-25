294c7099dc70cd89151a53368119c5f5ec32ab63
travelite <rendrabootcamp@gmail.com>
2023-03-26 05:58:42 +0700

    single number

---
+++ easy/singleNumber.js
@@ -0,0 +1,11 @@
+var singleNumber = function (nums) {
+  return nums.reduce((x, i) => ~x);
+};
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
+
+const a = 9;
+const b = 3;
+console.log(a ^ b);
