c76eeb3c05f0aa6d3973cea3eb3d9f8609c7a1b4
travelite <rendrabootcamp@gmail.com>
2023-06-20 00:10:22 +0700

    title to number

---
+++ easy/title-to-number.js
@@ -0,0 +1,17 @@
+var titleToNumber = function (columnTitle) {
+	// unfinished
+	let count = 0;
+	for (const title of columnTitle) {
+		if (columnTitle.length > 1) {
+			continue;
+			count += (columnTitle[0].charCodeAt(0) - 64) * 26;
+		} else {
+			count += title.charCodeAt(0) - 64;
+		}
+	}
+	return count;
+};
+
+console.log(titleToNumber("A"));
+console.log(titleToNumber("AB"));
+console.log(titleToNumber("ZY"));
